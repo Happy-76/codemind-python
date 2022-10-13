6aa94ef2652ce7071a39795d14c0d614c9c835fa
Mr. HAPPY <94671185+Happy-76@users.noreply.github.com>
2022-10-13 14:43:20 +0530

    l=input() p=l.split(" ") d=[] for i in p:     count=0     for j in i:         if j in "aeiouAEIOU":             count=count+1     d.append(count) for k in d:     print(k,end=" ")

---
+++ vowel_count_in_each_word.py
@@ -0,0 +1,11 @@
+l=input()
+p=l.split(" ")
+d=[]
+for i in p:
+    count=0
+    for j in i:
+        if j in "aeiouAEIOU":
+            count=count+1
+    d.append(count)
+for k in d:
+    print(k,end=" ")
